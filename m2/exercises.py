f087d197a678b9a589dfc0c8f05bbe4b091882b8
djengineer <djengineer9@gmail.com>
2020-07-09 21:58:41 +0800

    Create exercises.py

---
+++ m2/exercises.py
@@ -0,0 +1,17 @@
+import math
+
+### Equilateral Triangle
+
+s = 10
+area =  math.sqrt(3)/4*(s**2)
+print("Area of Equilateral triangle:")
+print(area)
+
+### Area of triangle with trigonometry
+
+a = 12
+b = 20
+C = math.pi/6
+area = (a*b*math.sin(C))/2
+print("Area of triangle with trigonometry:")
+print(area)
